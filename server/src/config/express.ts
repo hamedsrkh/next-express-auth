6c488c814f2be9c89ed8913a1ac01375a55d814e
hamedsrkh <ham.sarkhosh@gmail.com>
2024-02-21 22:47:09 +0330

    Added cors config to expressj project - Connected the register api to backend

--- server/src/config/express.ts
+++ server/src/config/express.ts
@@ -1,12 +1,18 @@
 import express from 'express'
 import passport from 'passport'
 import router from '../routes'
+import cors from 'cors'
 
 require('dotenv').config()
 const app = express()
 
 app.use(passport.initialize())
+// CORS options
+const corsOptions = {
+    origin: 'http://localhost:3000',
+};
 
+app.use(cors(corsOptions));
 app.use(express.json())
 app.use('/api', router)
 
