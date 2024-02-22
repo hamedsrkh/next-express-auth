da081130c4deefc3cf615d3d4874dea193dfb3c6
hamedsrkh <ham.sarkhosh@gmail.com>
2024-02-23 02:02:32 +0330

    Added controller to the project

---
+++ server/src/controllers/authController.ts
@@ -0,0 +1,60 @@
+import prisma from "@src/prisma/prismaClient";
+import {hashPassword} from "@src/authentication/auth";
+import {Request, Response} from "express";
+import bcrypt from "bcrypt";
+import {generateToken} from "@src/authentication/jwt";
+
+
+export async function register(req: Request, res: Response) {
+    const { email, password, name } = req.body
+    const existingUser = await prisma.user.findUnique({
+        where: { email },
+    })
+
+    if (existingUser) {
+        return res.status(400).json({ message: 'email already taken' })
+    }
+
+    // Hash the password
+    const hashedPassword = await hashPassword(password)
+
+    // Create the user
+    const newUser = await prisma.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        },
+    })
+
+    res.json({ message: 'User registered successfully', userId: newUser.id })
+}
+
+export async function login(req: Request, res: Response) {
+    const { email, password } = req.body
+    // Find the user by email
+    const user = await prisma.user.findUnique({
+        where: { email },
+    })
+
+    // Check if the user exists
+    if (!user) {
+        return res.status(401).json({ message: 'Invalid credentials' })
+    }
+
+    // Verify the password
+    const isPasswordValid = bcrypt.compareSync(String(password), user.password)
+
+    if (isPasswordValid) {
+        // Generate JWT token
+        const token = generateToken(user.id)
+        res.json({ id: user.id, email: user.email, name: user.name, accessToken: token })
+    } else {
+        res.status(401).json({ message: 'Invalid credentials' })
+    }
+}
+
+
+export async function getUser(req: Request, res: Response) {
+    res.json({ message: 'successful', user: req.user })
+}
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,62 +1,13 @@
 import express from 'express'
-import { generateToken } from '@src/authentication/jwt'
-import { authenticate, hashPassword } from '@src/authentication/auth'
-import bcrypt from 'bcrypt'
-import prisma from "@src/prisma/prismaClient";
+import { authenticate } from '@src/authentication/auth'
+import {getUser, login, register} from "@src/controllers/authController";
 
 const router = express.Router()
 
-router.post('/register', async (req, res) => {
-  const { email, password, name } = req.body
-  const existingUser = await prisma.user.findUnique({
-    where: { email },
-  })
+router.post('/register', register)
 
-  if (existingUser) {
-    return res.status(400).json({ message: 'email already taken' })
-  }
+router.post('/login', login)
 
-  // Hash the password
-  const hashedPassword = await hashPassword(password)
-
-  // Create the user
-  const newUser = await prisma.user.create({
-    data: {
-      name,
-      email,
-      password: hashedPassword,
-    },
-  })
-
-  res.json({ message: 'User registered successfully', userId: newUser.id })
-})
-
-router.post('/login', async (req, res) => {
-  const { email, password } = req.body
-  // Find the user by email
-  const user = await prisma.user.findUnique({
-    where: { email },
-  })
-
-  // Check if the user exists
-  if (!user) {
-    return res.status(401).json({ message: 'Invalid credentials' })
-  }
-
-  // Verify the password
-  const isPasswordValid = bcrypt.compareSync(String(password), user.password)
-
-  if (isPasswordValid) {
-    // Generate JWT token
-    const token = generateToken(user.id)
-    res.json({ id: user.id, email: user.email, name: user.name, accessToken: token })
-  } else {
-    res.status(401).json({ message: 'Invalid credentials' })
-  }
-})
-
-router.get('/user', authenticate, async (req, res) => {
-  res.json({ message: 'successful', user: req.user })
-})
+router.get('/user', authenticate, getUser)
 
 export default router
